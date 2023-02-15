6690bdbece8c3adbb2bf9c4f996e1deb1e6e9fbb
nurzhan <Salikh835@gmail.com>
2023-02-15 16:09:13 +0600

    done

--- src/main/java/lang/print/gaps/finalModuleTask/TemperatureConverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/TemperatureConverter.java
@@ -2,5 +2,7 @@ package lang.print.gaps.finalModuleTask;
 
 public class TemperatureConverter {
     public void toFahrenheit(int temperatureCelsius){
+        double result;
+        result=temperatureCelsius*1.8+32;
     }
 }
