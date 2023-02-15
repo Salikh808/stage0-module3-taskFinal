d190b5813512813e22d5e06b1d3557c1e7c55bba
nurzhan <Salikh835@gmail.com>
2023-02-15 15:05:39 +0600

    done

--- src/main/java/lang/print/gaps/finalModuleTask/DivisionByZero.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DivisionByZero.java
@@ -2,7 +2,8 @@ package lang.print.gaps.finalModuleTask;
 
 public class DivisionByZero {
     public static void main(String[] args) {
-        _ number = 10;
+        byte number = 10;
         System.out.println(number/0);
     }
 }
+//int, double, float, byte, short, long, boolean, char
