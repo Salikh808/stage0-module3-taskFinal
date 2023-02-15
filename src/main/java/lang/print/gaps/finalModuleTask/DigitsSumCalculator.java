f3e397efb3643492f9243f04124dcff8b15f5198
nurzhan <Salikh835@gmail.com>
2023-02-15 15:58:16 +0600

    done

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -2,5 +2,13 @@ package lang.print.gaps.finalModuleTask;
 
 public class DigitsSumCalculator {
     public void calculateSum(int number) {
+        int sum;
+        for(sum=0; number!=0; number=number/10)
+        {
+//finds the last digit and add it to the variable sum
+            sum = sum + number % 10;
+        }
+//prints the result
+        System.out.println("Sum of digits: "+sum);
     }
 }
